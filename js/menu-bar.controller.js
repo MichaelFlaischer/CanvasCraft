2a10a3c83b019433eee4a98c0aee650405860a37
nhftkfr25 <168108819+nhftkfr25@users.noreply.github.com>
2024-07-29 11:50:37 +0300

    Add interactive expandable/collapsible menu to the site
    - Implemented a new expandable/collapsible menu for the website to enhance user navigation.
    - Added `menuRender` function in `menu-bar-controller.js` to dynamically generate menu HTML.
    - Modified `main.services.js` to call `menuRender` on page load using `DOMContentLoaded` event.
    - Updated HTML to load scripts as ES6 modules.
    - Styled the menu with CSS for a smooth opening and closing transition.
    - Improved user experience with a responsive design and accessible navigation.
    
    This update improves the site's navigation, making it more user-friendly and intuitive.

---
+++ js/menu-bar.controller.js
@@ -0,0 +1,16 @@
+function menuRender() {
+  const elNnav = document.querySelector('nav')
+  elNnav.innerHTML = `
+        <input type="checkbox" id="active" />
+        <label for="active" class="menu-btn">
+          <i class="fas fa-bars"></i>
+        </label>
+        <div class="nav-container">
+          <ul>
+            <li><a href="main.html">Home</a></li>
+            <li><a href="canvas.html">Canvas</a></li>
+            <li><a href="gallery.html">Gallery</a></li>
+          </ul>
+        </div>
+      `
+}
