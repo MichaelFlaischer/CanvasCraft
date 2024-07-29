41f6152bcd3007d388c75d6ac632a0a1ba6ba849
nhftkfr25 <168108819+nhftkfr25@users.noreply.github.com>
2024-07-29 18:35:55 +0300

    Clear Canvas Functionality and download Canvas as Image
    Clear Canvas Functionality:
    
    A new "Clear Canvas" feature has been added, allowing users to easily clear the entire canvas with a single action. This helps in quickly resetting the canvas for new drawings without having to refresh the page.
    Download Canvas as Image:
    
    Users can now download their drawings as a PNG image file. A new button has been added to facilitate this feature, providing an easy way to save the artwork created on the canvas.

--- js/canvas.controller.js
+++ js/canvas.controller.js
@@ -10,3 +10,14 @@ function clearCanvas() {
   gCtx.fillStyle = '#fdfdfd'
   gCtx.fillRect(0, 0, gElCanvas.width, gElCanvas.height)
 }
+
+function downloadCanvas() {
+  const canvas = document.getElementById('canvas')
+  const dataURL = canvas.toDataURL('image/png')
+  const link = document.createElement('a')
+  link.href = dataURL
+  link.download = 'canvas_drawing.png'
+  document.body.appendChild(link)
+  link.click()
+  document.body.removeChild(link)
+}
